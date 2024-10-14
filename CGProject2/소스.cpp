19557c01e9c361bc405bf0f4cd17c95e96931400
RcoasteR0 <st_anthony@naver.com>
2024-10-14 14:39:07 +0900

    문제1: 코드 갈아엎음

--- CGProject2/소스.cpp
+++ CGProject2/소스.cpp
@@ -9,268 +9,339 @@
 
 using namespace std;
 
-random_device rd;
-mt19937 gen(rd());
-
-void InitBuffer();
-void UpdateBuffer();
-GLvoid drawScene();
-GLvoid Reshape(int w, int h);
-GLvoid Keyboard(unsigned char key, int x, int y);
-GLvoid Mouse(int button, int state, int x, int y);
-GLvoid Timer(int value);
-
+#define SHAPES 10
+GLfloat triShape[3][3]; //--- 삼각형 위치 값
+GLfloat colors[3][3]; //--- 삼각형 꼭지점 색상
 GLuint vao, vbo[2], ebo;
+GLvoid drawScene(GLvoid);
+void convertXY(int x, int y, float& fx, float& fy);
+int random();
+void UpdateBuffer();
+void Color(float& color);
+GLvoid Mouse(int button, int state, int x, int y);
+void InitBuffer();
+void make_vertexShaders();
+void make_fragmentShaders();
+void make_shaderProgram();
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+GLvoid Keyboard(unsigned char key, int x, int y);
+float bGCr = 1.0, bGCg = 1.0, bGCb = 1.0;
+GLuint shaderPID;
+GLvoid Reshape(int w, int h);
+#define WIDTH 800
+#define HEIGHT 600
 
-struct Coordinate { GLfloat x = 0, y = 0, z = 0; };
-struct RGB { GLfloat Red = 0, Green = 0, Blue = 0; };
-
-class Point
+struct Shape
 {
-public:
-	Coordinate Shape;
-	RGB colors;
-
-	Point() {}
-
-	Point(Coordinate coord, RGB rgb)
-	{
-		Shape = coord;
-		colors = rgb;
-	}
-
-	~Point() {}
-
-	void GetCoord(GLfloat coord[])
-	{
-		coord[0] = Shape.x;
-		coord[1] = Shape.y;
-		coord[2] = Shape.z;
-	}
-
-	void GetColor(GLfloat color[])
-	{
-		color[0] = colors.Red;
-		color[1] = colors.Green;
-		color[2] = colors.Blue;
-	}
+	GLfloat shapecoord[4][3] = { { 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 }, { 0.0, 0.0, 0.0 }, { 0, 0, 0} };
+	int state = 0;
+	bool states = false;
 };
 
-class Line
-{
-public:
-	Coordinate Shape[2];
-	RGB colors;
-
-	Line() {}
-
-	Line(Coordinate coord[2], RGB rgb)
-	{
-		Shape[0] = coord[0];
-		Shape[1] = coord[1];
-		colors = rgb;
-	}
-
-	~Line() {}
-
-	GLfloat* GetCoord()
-	{
-		static GLfloat coord[6] = { Shape[0].x, Shape[0].y, Shape[0].z, 
-							Shape[1].x, Shape[1].y, Shape[1].z };
-		return coord;
-	}
-
-	GLfloat* GetColor()
-	{
-		static GLfloat color[6] = { colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue };
-		return color;
-	}
-
-};
-
-class Triangle
-{
-public:
-	Coordinate Shape[3];
-	RGB colors;
-
-	Triangle() {}
-
-	Triangle(Coordinate coord[3], RGB rgb)
-	{
-		Shape[0] = coord[0];
-		Shape[1] = coord[1];
-		Shape[2] = coord[2];
-		colors = rgb;
-	}
-
-	~Triangle() {}
-
-	GLfloat* GetCoord()
-	{
-		static GLfloat coord[9] = { Shape[0].x, Shape[0].y, Shape[0].z,
-							Shape[1].x, Shape[1].y, Shape[1].z,
-							Shape[2].x, Shape[2].y, Shape[2].z };
-		return coord;
-	}
-
-	GLfloat* GetColor()
-	{
-		static GLfloat color[9] = { colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue };
-		return color;
-	}
-
-};
-
-class REctangle
-{
-public:
-	Coordinate Shape[4];
-	RGB colors;
-
-	REctangle() {}
-
-	REctangle(Coordinate coord[4], RGB rgb)
-	{
-		Shape[0] = coord[0];
-		Shape[1] = coord[1];
-		Shape[2] = coord[2];
-		Shape[3] = coord[3];
-		colors = rgb;
-	}
-
-	~REctangle() {}
-
-	GLfloat* GetCoord()
-	{
-		static GLfloat coord[12] = { Shape[0].x, Shape[0].y, Shape[0].z,
-							Shape[1].x, Shape[1].y, Shape[1].z, 
-							Shape[2].x, Shape[2].y, Shape[2].z, 
-							Shape[3].x, Shape[3].y, Shape[3].z };
-		return coord;
-	}
-
-	GLfloat* GetColor()
-	{
-		static GLfloat color[12] = { colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue,
-							colors.Red, colors.Green, colors.Blue };
-		return color;
-	}
-
-};
-
-#ifdef Quiz1
-enum Shape { NONE = -1, POiNT, LINE, TRIANGLE, RECTANGLE };
-Point points[10];
-Line lines[10];
-Triangle triangles[10];
-REctangle rects[10];
 Shape shape[10];
-Shape selectedshape;
-int shapecount;
-int shapeindex;
-int drawindex;
+int shapecount = 0;
 
-uniform_real_distribution<GLfloat> randcolor(0.0f, 1.0f);
-
-RGB RandomColor()
+void main(int argc, char** argv)
 {
-	return { randcolor(gen), randcolor(gen) , randcolor(gen) };
-}
-
-void Initialize()
-{
-	for (int i = 0; i < 10; ++i)
-	{
-		points[i] = Point();
-		lines[i] = Line();
-		triangles[i] = Triangle();
-		rects[i] = REctangle();
-		shape[i] = NONE;
-	}
-
-	selectedshape = POiNT;
-	shapecount = 0;
-	shapeindex = 0;
-}
-#endif // Quiz1
-
-//메인 함수
-void main (int argc, char** argv)	//윈도우 출력하고 콜백함수 설정
-{
-	//윈도우 생성하기
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-	glutCreateWindow("Example1");
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow("Example7");
 
-	//GLEW 초기화하기
 	glewExperimental = GL_TRUE;
-	glewInit();
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Unable to initialize GLEW" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	else
+		std::cout << "GLEW Initialized\n";
 
-	//세이더 읽어와서 세이더 프로그램 만들기
-	make_shaderProgram();		//세이더 프로그램 만들기
+	make_shaderProgram();
 	InitBuffer();
-
-#ifdef Quiz1
-	Initialize();
-#endif // Quiz1
-
-	glutDisplayFunc(drawScene);		//출력 콜백 함수
-	glutReshapeFunc(Reshape);
+	glutDisplayFunc(drawScene);
 	glutKeyboardFunc(Keyboard);
 	glutMouseFunc(Mouse);
+	glutReshapeFunc(Reshape);
 	glutMainLoop();
 }
 
-//--- 버퍼 생성하고 데이터 받아오기
+GLvoid drawScene()
+{
+	glClearColor(bGCr, bGCg, bGCb, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glUseProgram(shaderProgramID);
+	UpdateBuffer();
+	glBindVertexArray(vao);
+	for (int i = 0; i < 10; i++)
+	{
+		if (shape[i].states)
+		{
+			if (shape[i].state == 1)
+			{
+				glPointSize(5.0);
+				glDrawElements(GL_POINTS, 1, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
+			}
+			else if (shape[i].state == 2)
+			{
+				glDrawElements(GL_LINES, 2, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
+			}
+			else if (shape[i].state == 3)
+			{
+				glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
+			}
+			else if (shape[i].state == 4)
+			{
+				glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
+			}
+		}
+	}
+	glutSwapBuffers();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+}
+
+void convertXY(int x, int y, float& fx, float& fy)
+{
+	int w = WIDTH;
+	int h = HEIGHT;
+	fx = (float)((x - (float)w / 2.0) * (float)(1.0 / (float)(w / 2.0)));
+	fy = -(float)((y - (float)h / 2.0) * (float)(1.0 / (float)(h / 2.0)));
+}
+
+int random()
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<int> random(0, 9);
+	return random(gen);
+}
+
+void Color(float& color)
+{
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> random(0, 1);
+	color = random(gen);
+}
+
+GLvoid Keyboard(unsigned char key, int x, int y)
+{
+	int a = random();
+	switch (key)
+	{
+	case 'p':
+		shape[shapecount].state = 1;
+		break;
+	case 'l':
+		shape[shapecount].state = 2;
+		break;
+	case 't':
+		shape[shapecount].state = 3;
+		break;
+	case 'r':
+		shape[shapecount].state = 4;
+		break;
+	case 'w':
+		switch (shape[a].state)
+		{
+		case 1:
+			shape[a].shapecoord[0][1] += 0.05;
+			break;
+		case 2:
+			shape[a].shapecoord[0][1] += 0.05;
+			shape[a].shapecoord[1][1] += 0.05;
+			break;
+		case 3:
+			shape[a].shapecoord[0][1] += 0.05;
+			shape[a].shapecoord[1][1] += 0.05;
+			shape[a].shapecoord[2][1] += 0.05;
+			break;
+		case 4:
+			shape[a].shapecoord[0][1] += 0.05;
+			shape[a].shapecoord[1][1] += 0.05;
+			shape[a].shapecoord[2][1] += 0.05;
+			shape[a].shapecoord[3][1] += 0.05;
+			break;
+		}
+		break;
+	case 'a':
+		switch (shape[a].state)
+		{
+		case 1:
+			shape[a].shapecoord[0][0] -= 0.05;
+			break;
+		case 2:
+			shape[a].shapecoord[0][0] -= 0.05;
+			shape[a].shapecoord[1][0] -= 0.05;
+			break;
+		case 3:
+			shape[a].shapecoord[0][0] -= 0.05;
+			shape[a].shapecoord[1][0] -= 0.05;
+			shape[a].shapecoord[2][0] -= 0.05;
+			break;
+		case 4:
+			shape[a].shapecoord[0][0] -= 0.05;
+			shape[a].shapecoord[1][0] -= 0.05;
+			shape[a].shapecoord[2][0] -= 0.05;
+			shape[a].shapecoord[3][0] -= 0.05;
+			break;
+		}
+		break;
+	case 's':
+		switch (shape[a].state)
+		{
+		case 1:
+			shape[a].shapecoord[0][1] -= 0.05;
+			break;
+		case 2:
+			shape[a].shapecoord[0][1] -= 0.05;
+			shape[a].shapecoord[1][1] -= 0.05;
+			break;
+		case 3:
+			shape[a].shapecoord[0][1] -= 0.05;
+			shape[a].shapecoord[1][1] -= 0.05;
+			shape[a].shapecoord[2][1] -= 0.05;
+			break;
+		case 4:
+			shape[a].shapecoord[0][1] -= 0.05;
+			shape[a].shapecoord[1][1] -= 0.05;
+			shape[a].shapecoord[2][1] -= 0.05;
+			shape[a].shapecoord[3][1] -= 0.05;
+			break;
+		}
+		break;
+	case 'd':
+		switch (shape[a].state)
+		{
+		case 1:
+			shape[a].shapecoord[0][0] += 0.05;
+			break;
+		case 2:
+			shape[a].shapecoord[0][0] += 0.05;
+			shape[a].shapecoord[1][0] += 0.05;
+			break;
+		case 3:
+			shape[a].shapecoord[0][0] += 0.05;
+			shape[a].shapecoord[1][0] += 0.05;
+			shape[a].shapecoord[2][0] += 0.05;
+			break;
+		case 4:
+			shape[a].shapecoord[0][0] += 0.05;
+			shape[a].shapecoord[1][0] += 0.05;
+			shape[a].shapecoord[2][0] += 0.05;
+			shape[a].shapecoord[3][0] += 0.05;
+			break;
+		}
+		break;
+	case 'c':
+		shapecount = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			shape[i].state = 0;
+			shape[i].states = false;
+		}
+		break;
+	case 'q':
+		glutLeaveMainLoop();
+		break;
+	}
+	glutPostRedisplay();
+}
+
+GLvoid Mouse(int button, int state, int x, int y)
+{
+	float fx = 0.0, fy = 0.0;
+	convertXY(x, y, fx, fy);
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		if (shapecount < 10)
+		{
+			switch (shape[shapecount].state)
+			{
+			case 1:
+				shape[shapecount].shapecoord[0][0] = fx;
+				shape[shapecount].shapecoord[0][1] = fy;
+				break;
+			case 2:
+				shape[shapecount].shapecoord[0][0] = fx - 0.25;
+				shape[shapecount].shapecoord[0][1] = fy - 0.25;
+				shape[shapecount].shapecoord[1][0] = fx + 0.25;
+				shape[shapecount].shapecoord[1][1] = fy + 0.25;
+				break;
+			case 3:
+				shape[shapecount].shapecoord[0][0] = fx;
+				shape[shapecount].shapecoord[0][1] = fy;
+				shape[shapecount].shapecoord[1][0] = fx - 0.1;
+				shape[shapecount].shapecoord[1][1] = fy - 0.3;
+				shape[shapecount].shapecoord[2][0] = fx + 0.1;
+				shape[shapecount].shapecoord[2][1] = fy - 0.3;
+				break;
+			case 4:
+				shape[shapecount].shapecoord[0][0] = fx - 0.25;
+				shape[shapecount].shapecoord[0][1] = fy - 0.25;
+				shape[shapecount].shapecoord[1][0] = fx + 0.25;
+				shape[shapecount].shapecoord[1][1] = fy - 0.25;
+				shape[shapecount].shapecoord[2][0] = fx - 0.25;
+				shape[shapecount].shapecoord[2][1] = fy + 0.25;
+				shape[shapecount].shapecoord[3][0] = fx + 0.25;
+				shape[shapecount].shapecoord[3][1] = fy + 0.25;
+				break;
+			}
+			shape[shapecount].states = true;
+			shapecount++;
+			shape[shapecount].state = shape[shapecount - 1].state;
+		}
+	}
+	glutPostRedisplay();
+}
+
 void InitBuffer()
 {
-	glGenVertexArrays(1, &vao); //VAO 를 지정하고 할당하기
+	glGenVertexArrays(1, &vao); //--- VAO 를 지정하고 할당하기
+	glBindVertexArray(vao); //--- VAO를 바인드하기
 
-	glBindVertexArray(vao); //VAO 를 바인드하기
 
-	glGenBuffers(2, vbo); //2 개의 VBO 를 지정하고 할당하기
-
-	//1 번째 VBO 를 활성화하여 바인드하고 , 버텍스 속성 좌표값 을 저장
+	glGenBuffers(2, vbo); //--- 2개의 VBO를 지정하고 할당하기
+	//--- 1번째 VBO를 활성화하여 바인드하고, 버텍스 속성 (좌표값)을 저장
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-
-	//변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다
-#ifdef Quiz1
-	glBufferData(GL_ARRAY_BUFFER, 10 * 12 * sizeof(GLfloat), NULL, GL_STATIC_DRAW);
-#endif // Quiz1
-
-	//좌표값을 attribute 인덱스 0 번에 명시한다 : 버텍스 당 3 * float
+	//--- 변수 diamond 에서 버텍스 데이터 값을 버퍼에 복사한다.
+	//--- triShape 배열의 사이즈: 9 * float
+	glBufferData(GL_ARRAY_BUFFER, SHAPES * 12 * sizeof(GLfloat), NULL, GL_DYNAMIC_DRAW);
+	//--- 좌표값을 attribute 인덱스 0번에 명시한다: 버텍스 당 3* float
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	//attribute 인덱스 0 번을 사용가능하게 함
+	//--- attribute 인덱스 0번을 사용가능하게 함
 	glEnableVertexAttribArray(0);
 
-	//2 번째 VBO 를 활성화 하여 바인드 하고 , 버텍스 속성 색상 을 저장
+	//--- 2번째 VBO를 활성화 하여 바인드 하고, 버텍스 속성 (색상)을 저장
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-
-	//변수 colors 에서 버텍스 색상을 복사한다
-#ifdef Quiz1
-	glBufferData(GL_ARRAY_BUFFER, 10 * 12 * sizeof(GLfloat), NULL, GL_STATIC_DRAW);
-#endif // Quiz1
-
-	//색상값을 attribute 인덱스 1 번에 명시한다 : 버텍스 당 3 * float
+	//--- 변수 colors에서 버텍스 색상을 복사한다.
+	//--- colors 배열의 사이즈: 9 *float
+	glBufferData(GL_ARRAY_BUFFER, SHAPES * 12 * sizeof(GLfloat), NULL, GL_STATIC_DRAW);
+	for (int i = 0; i < SHAPES; i++)
+	{
+		for (int z = 0; z < 3; z++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				Color(colors[z][j]);
+			}
+		}
+		glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 12 * sizeof(GLfloat), colors);
+	}
+	//--- 색상값을 attribute 인덱스 1번에 명시한다: 버텍스 당 3*float
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
-
-	//attribute 인덱스 1 번을 사용 가능하게 함
+	//--- attribute 인덱스 1번을 사용 가능하게 함.
 	glEnableVertexAttribArray(1);
 
-#ifdef Quiz1
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	GLuint indices[10 * 4];
-	for (int i = 0; i < 10; i++)
+	GLuint indices[SHAPES * 4];
+	for (int i = 0; i < SHAPES; i++)
 	{
 		indices[i * 4] = i * 4;
 		indices[i * 4 + 1] = i * 4 + 1;
@@ -278,188 +349,13 @@ void InitBuffer()
 		indices[i * 4 + 3] = i * 4 + 3;
 	}
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-#endif // Quiz1
-
 }
 
 void UpdateBuffer()
 {
-#ifdef Quiz1
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
-	for (int i = 0; i < shapecount; i++)
+	for (int i = 0; i < SHAPES; i++)
 	{
-		GLfloat temp[12];
-		switch (shape[i])
-		{
-		case POiNT:
-			points[i].GetCoord(temp);
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 3 * sizeof(GLfloat), temp);
-			break;
-		case LINE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 6 * sizeof(GLfloat), lines[i].GetCoord());
-			break;
-		case TRIANGLE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 9 * sizeof(GLfloat), triangles[i].GetCoord());
-			break;
-		case RECTANGLE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 12 * sizeof(GLfloat), rects[i].GetCoord());
-			break;
-		default:
-			break;
-		}
+		glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 12 * sizeof(GLfloat), shape[i].shapecoord);
 	}
-
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
-	for (int i = 0; i < shapecount; i++)
-	{
-		GLfloat temp[12];
-		switch (shape[i])
-		{
-		case POiNT:
-			points[i].GetColor(temp);
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 3 * sizeof(GLfloat), temp);
-			break;
-		case LINE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 6 * sizeof(GLfloat), lines[i].GetColor());
-			break;
-		case TRIANGLE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 9 * sizeof(GLfloat), triangles[i].GetColor());
-			break;
-		case RECTANGLE:
-			glBufferSubData(GL_ARRAY_BUFFER, i * 12 * sizeof(GLfloat), 12 * sizeof(GLfloat), rects[i].GetColor());
-			break;
-		default:
-			break;
-		}
-	}
-#endif // Quiz1
-
-}
-
-//--- 출력 콜백 함수
-GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
-{
-	GLfloat rColor, gColor, bColor;
-
-	rColor = gColor = bColor = 1.0; //--- 배경색을 하양색으로 설정
-	glClearColor(rColor, gColor, bColor, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glUseProgram(shaderProgramID);
-	UpdateBuffer();
-
-	glBindVertexArray(vao);
-#ifdef Quiz1
-	for (int i = 0; i < shapecount; ++i)
-	{
-		switch (shape[i])
-		{
-		case POiNT:
-			glPointSize(5.0f);
-			glDrawElements(GL_POINT, 1, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
-			cout << "draw point" << points[i].Shape.x << " " << points[i].Shape.y << endl;
-			break;
-		case LINE:
-			glDrawElements(GL_LINES, 2, GL_UNSIGNED_INT, (void*)(i * 4 * sizeof(GLuint)));
-			break;
-		case TRIANGLE:
-			break;
-		case RECTANGLE:
-			break;
-		default:
-			break;
-		}
-	}
-#endif // Quiz1
-
-	glutSwapBuffers(); // 화면에 출력하기
-}
-
-//--- 다시그리기 콜백 함수
-GLvoid Reshape(int w, int h) //--- 콜백 함수: 다시 그리기 콜백 함수
-{
-	glViewport(0, 0, w, h);
-}
-
-GLvoid Keyboard(unsigned char key, int x, int y)
-{
-#ifdef Quiz1
-	switch (key)
-	{
-	case 'p':
-		selectedshape = POiNT;
-		break;
-	case 'l':
-		selectedshape = LINE;
-		break;
-	case 't':
-		selectedshape = TRIANGLE;
-		break;
-	case 'r':
-		selectedshape = RECTANGLE;
-		break;
-	case 'w':
-		break;
-	case 'a':
-		break;
-	case 's':
-		break;
-	case 'd':
-		break;
-	case 'c':
-		Initialize();
-		break;
-	default:
-		break;
-	}
-#endif // Quiz1
-
-	//프로그램 종료
-	if (key == 'q')
-		glutLeaveMainLoop();
-
-	glutPostRedisplay();
-}
-
-GLvoid Mouse(int button, int state, int x, int y)
-{
-	GLfloat mouseX = (x - WINDOW_WIDTH / 2.0f) / (WINDOW_WIDTH / 2.0f);
-	GLfloat mouseY = (WINDOW_HEIGHT / 2.0f - y) / (WINDOW_HEIGHT / 2.0f);
-
-#ifdef Quiz1
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN && shapecount < 10)
-	{
-		switch (selectedshape)
-		{
-		case POiNT:
-		{
-			points[shapecount] = Point({ mouseX, mouseY, 0.0f }, RandomColor());
-			shape[shapecount] = POiNT;
-			shapecount++;
-			break;
-		}
-		case LINE:
-		{
-			Coordinate c[2] = {{mouseX - 0.05f, mouseY - 0.05f, 0.0f}, {mouseX + 0.05f, mouseY + 0.05f, 0.0f}};
-			lines[shapecount] = Line(c, RandomColor());
-			shape[shapecount] = LINE;
-			shapecount++;
-			break;
-		}
-		case TRIANGLE:
-		{
-
-			break;
-		}
-		case RECTANGLE:
-		{
-
-			break;
-		}
-		default:
-			break;
-		}
-	}
-
-#endif // Quiz1
 }
